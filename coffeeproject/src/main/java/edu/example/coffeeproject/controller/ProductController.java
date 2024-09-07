0ea880edc2d8d61711ed2b14084fddd6e962f628
kimdongjoon-web <color6763@naver.com>
2024-09-07 20:06:23 +0900

    test initial REST API

--- coffeeproject/src/main/java/edu/example/coffeeproject/controller/ProductController.java
+++ coffeeproject/src/main/java/edu/example/coffeeproject/controller/ProductController.java
@@ -4,20 +4,30 @@ package edu.example.coffeeproject.controller;
 import edu.example.coffeeproject.entity.Product;
 import edu.example.coffeeproject.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/products")
 public class ProductController {
 
     @Autowired
     ProductService productService;
 
-    @GetMapping("/products/list")
+    @PostMapping("/create")
+    public Product createProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+
+    @GetMapping
     public List<Product> getAllProducts() {
         List<Product> productList = productService.getAllProducts();
         return productList;
+    }
+
+    @DeleteMapping("/{productId}")
+    public void deleteProduct(@PathVariable Long productId) {
+        productService.deleteProduct(productId);
     }
 }
--- coffeeproject/src/main/java/edu/example/coffeeproject/service/ProductService.java
+++ coffeeproject/src/main/java/edu/example/coffeeproject/service/ProductService.java
@@ -2,7 +2,6 @@ package edu.example.coffeeproject.service;
 
 import edu.example.coffeeproject.entity.Product;
 import edu.example.coffeeproject.repository.ProductRepository;
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,21 +13,15 @@ public class ProductService {
     @Autowired
     ProductRepository productRepository;
 
-    public ProductService(ProductRepository productRepository) {
-        this.productRepository = productRepository;
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
     }
 
     public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
 
-    @Transactional
-    public void initializeProducts() {
-        if (productRepository.count() == 0) {  // 데이터가 없을 때만 삽입
-            productRepository.save(new Product(null, "Columbia Coffee", 10000, "콜롬비아의 맛있는 커피", null, null));
-            productRepository.save(new Product(null, "Columbia Quindio", 20000, "콜롬비아산 Quindio 커피", null, null));
-            productRepository.save(new Product(null, "Brazil Serra Do Coffee", 15000, "브라질산 풍미가득한 커피", null, null));
-            productRepository.save(new Product(null, "Ethiopia Sidamo", 8000, "에디오피아 커피 빈", null, null));
-        }
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
     }
 }
